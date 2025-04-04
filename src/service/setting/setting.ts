5e47de3ea67bd60a999374b0c854431aca2090e5
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-04-05 00:52:56 +0900

    refactor: settings 쿼리옵션 타입추론 되도록 settings api 함수 리턴타입 지정

--- src/service/setting/setting.ts
+++ src/service/setting/setting.ts
@@ -1,9 +1,17 @@
+import {
+  PositionItem,
+  ResponseBody,
+  TechStackCategory,
+  TechStackItem,
+  TechStackWithCategory,
+} from '@/utils/type';
+
 const publicURL = process.env.NEXT_PUBLIC_URL;
 
 /**
  * 포지션 목록 조회
  */
-export async function getPositionList() {
+export async function getPositionList(): Promise<ResponseBody<PositionItem[]>> {
   const response = await fetch(`${publicURL}/api/setting/position`);
   return await response.json();
 }
@@ -11,7 +19,9 @@ export async function getPositionList() {
 /**
  * 기술스택 목록 조회
  */
-export async function getTechStackList() {
+export async function getTechStackList(): Promise<
+  ResponseBody<TechStackItem[]>
+> {
   const response = await fetch(`${publicURL}/api/setting/tech-stack`);
   return await response.json();
 }
@@ -19,7 +29,9 @@ export async function getTechStackList() {
 /**
  * 기술스택 카테고리 목록 조회
  */
-export async function getTechStackCategoryList() {
+export async function getTechStackCategoryList(): Promise<
+  ResponseBody<TechStackCategory[]>
+> {
   const response = await fetch(`${publicURL}/api/setting/tech-stack-category`);
   return await response.json();
 }
@@ -27,7 +39,9 @@ export async function getTechStackCategoryList() {
 /**
  * 기술스택 카테고리-기술스택 목록 조회
  */
-export async function getTechStackListWithCategory() {
+export async function getTechStackListWithCategory(): Promise<
+  ResponseBody<TechStackWithCategory[]>
+> {
   const response = await fetch(
     `${publicURL}/api/setting/tech-stack-with-category`,
   );
