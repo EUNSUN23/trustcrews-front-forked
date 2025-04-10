316d1d68c97bb75ac2de465be414a31dd1081113
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-04-10 21:17:20 +0900

    refactor: projectApplyStatus, taksStatus 뱃지 공통 variants 적용

--- src/components/ui/badge/ProjectApplyStatusBadge.tsx
+++ src/components/ui/badge/ProjectApplyStatusBadge.tsx
@@ -2,6 +2,8 @@ import cn from '@/utils/cn';
 import { ProjectApplyStatusData } from '@/features/board/projectApplyStatus/type';
 import { clsx } from 'clsx';
 import { HTMLAttributes } from 'react';
+import { BadgeVariants } from '@/utils/badge';
+import { VariantProps } from 'class-variance-authority';
 
 const STATUS_COLOR = {
   WAIT: 'bg-gray-50 ring-gray-500/10 text-gray-600',
@@ -13,26 +15,35 @@ const { WAIT, ACCEPT, REJECT } = STATUS_COLOR;
 const projectApplyStatusBadgeClass = (
   status: ProjectApplyStatusData['status'],
 ) =>
-  clsx(
+  clsx({
+    [WAIT]: status.code === 'PAS1001',
+    [ACCEPT]: status.code === 'PAS1002',
+    [REJECT]: status.code === 'PAS1003',
+  });
+
+const ProjectApplyStatusBadgeVariants = (
+  status: ProjectApplyStatusData['status'],
+) =>
+  BadgeVariants(
     'mr-2 inline-flex items-center rounded-md font-medium ring-1 ring-inset',
-    {
-      [WAIT]: status.code === 'PAS1001',
-      [ACCEPT]: status.code === 'PAS1002',
-      [REJECT]: status.code === 'PAS1003',
-    },
+    projectApplyStatusBadgeClass(status),
   );
 
-interface ProjectApplyStatusBadgeProps extends HTMLAttributes<HTMLSpanElement> {
+interface ProjectApplyStatusBadgeProps
+  extends HTMLAttributes<HTMLSpanElement>,
+    VariantProps<ReturnType<typeof ProjectApplyStatusBadgeVariants>> {
   status: ProjectApplyStatusData['status'];
 }
 
 export function ProjectApplyStatusBadge({
   status,
+  size,
   ...props
 }: ProjectApplyStatusBadgeProps) {
   return (
     <span
-      className={cn(projectApplyStatusBadgeClass(status), props.className)}
+      {...props}
+      className={cn(ProjectApplyStatusBadgeVariants(status)({ size }))}
       aria-hidden={true}
     >
       {status.name}
--- src/components/ui/badge/TaskStatusBadge.tsx
+++ src/components/ui/badge/TaskStatusBadge.tsx
@@ -5,7 +5,8 @@ import {
 } from '@/app/project/@task/_utils/type';
 import cn from '@/utils/cn';
 import { HTMLAttributes } from 'react';
-import { cva, VariantProps } from 'class-variance-authority';
+import { BadgeVariants } from '@/utils/badge';
+import { VariantProps } from 'class-variance-authority';
 
 const STATUS_COLOR = {
   STANDBY: 'bg-grey900 text-grey000',
@@ -22,16 +23,7 @@ const taskStatusBadgeClass = (statusCode: TaskStatusValueType) =>
   });
 
 const TaskStatusBadgeVariants = (statusCode: TaskStatusValueType) =>
-  cva(taskStatusBadgeClass(statusCode), {
-    variants: {
-      size: {
-        xs: 'text-[11px] mobile:text-[9px] px-2 mobile:px-1 py-0.5 mobile:py-0',
-        sm: 'tablet:text-sm mobile:text-xs px-2 py-1',
-        md: 'tablet:text-lg mobile:text-base tablet:px-8 mobile:px-4 tablet:py-4 mobile:py-2',
-        lg: 'text-lg px-8 py-4',
-      },
-    },
-  });
+  BadgeVariants(taskStatusBadgeClass(statusCode));
 
 interface TaskStatusBadgeProps
   extends HTMLAttributes<HTMLSpanElement>,
