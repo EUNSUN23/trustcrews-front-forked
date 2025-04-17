dd9d8fccbf4ef4893191e48d15bc708e2fbddd6b
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-04-18 00:32:00 +0900

    refactor: myProjects 관련 store,service,component feature/project/auth/myProjects로 이동

--- src/features/project/auth/myProjects/components/MyProjects.tsx
+++ src/features/project/auth/myProjects/components/MyProjects.tsx
@@ -2,49 +2,37 @@
 
 import { useState } from 'react';
 import ProjectCard from './ProjectCard';
-import { useQuery } from '@tanstack/react-query';
-import { PageResponseBody, ProjectInfoSummary } from '@/utils/type';
 import CommonPagination from '@/components/ui/CommonPagination';
 import { ITEM_COUNT, PAGE_RANGE } from '@/utils/constant';
-import { getMyProjectList } from './service';
-import CardListSkeleton from '@/components/ui/skeleton/CardListSkeleton';
+import { useMyProjects } from '@/features/project/auth/myProjects/service/getMyProjects';
 
 function MyProjects() {
   const [pageNumber, setPageNumber] = useState(0);
 
-  function onChangePageHandler(pageNumber: number) {
+  const handleChangePage = (pageNumber: number) => {
     setPageNumber(pageNumber - 1);
-  }
+  };
 
-  const { data, isFetching, isError } = useQuery<
-    Promise<PageResponseBody<ProjectInfoSummary[]>>,
-    Error,
-    PageResponseBody<ProjectInfoSummary[]>
-  >({
-    queryKey: ['myProjectList', pageNumber],
-    queryFn: () => getMyProjectList(pageNumber, 8),
-    staleTime: 0,
-  });
+  const {
+    data: {
+      data: { content, totalPages },
+    },
+  } = useMyProjects(pageNumber);
 
-  if (isFetching || isError) return <CardListSkeleton itemCount={8} />;
-
-  if (!data?.data || data.data.content.length < 1)
+  if (totalPages === 0)
     return (
-      <div className='flex items-center justify-center w-full h-[280px] bg-ground100 text-center rounded-md'>
-        <p className='py-10 mobile:text-2xl tablet:text-3xl font-medium text-grey900'>
+      <div className='flex items-center justify-center w-full h-[260px] bg-ground100 text-center rounded-md my-5'>
+        <p className='py-10 mobile:text-2xl tablet:text-3xl font-medium text-grey900 px-3'>
           참여하고 있는 프로젝트가 없습니다.
         </p>
       </div>
     );
 
-  const projectPosts = data.data.content;
-  const totalCount = data.data.totalPages;
-
   return (
     <section className='my-10'>
       <h2 className='sr-only'>참여 프로젝트</h2>
       <ul className='grid justify-items-center pc:grid-cols-4 tablet:grid-cols-2 mobile:grid-cols-1 mt-8 mobile:mt-2 gap-10'>
-        {projectPosts.map((v) => (
+        {content.map((v) => (
           <li
             key={v.projectId}
             className='flex-col w-[280px] max-h-[330px] rounded-xl border-2 shadow-lg mobile:bg-white mobile:w-full mobile:mt-2'
@@ -56,9 +44,9 @@ function MyProjects() {
       <CommonPagination
         activePage={pageNumber + 1}
         itemsCountPerPage={ITEM_COUNT.CARDS}
-        totalItemsCount={totalCount}
+        totalItemsCount={totalPages}
         pageRangeDisplayed={PAGE_RANGE.DEFAULT}
-        onChangePageHandler={onChangePageHandler}
+        onChangePageHandler={handleChangePage}
       />
     </section>
   );
---
+++ src/features/project/auth/myProjects/service/getMyProjects.ts
@@ -0,0 +1,39 @@
+import { request } from '@/lib/clientApi/request';
+import { sortByStartDate } from '@/utils/common';
+import { PageResponseBody, ProjectInfoSummary } from '@/utils/type';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { ITEM_COUNT } from '@/utils/constant';
+
+/**
+ * 참여 프로젝트 목록 조회
+ */
+export const getMyProjects = async (
+  pageIndex: number,
+  itemCount: number,
+): Promise<PageResponseBody<ProjectInfoSummary[]>> => {
+  const resBody = await request(
+    'GET',
+    `/api/project/list?pageIndex=${pageIndex}&itemCount=${itemCount}`,
+  );
+
+  return {
+    ...resBody,
+    data: resBody.data
+      ? {
+          totalPages: resBody.data.totalPages,
+          content: resBody.data.content
+            ? sortByStartDate(resBody.data.content, 'desc')
+            : [],
+        }
+      : null,
+  };
+};
+
+export const getMyProjectsQueryKey = ['myProjects'];
+
+export const useMyProjects = (pageNumber: number) => {
+  return useSuspenseQuery({
+    queryKey: [...getMyProjectsQueryKey, pageNumber],
+    queryFn: () => getMyProjects(pageNumber, ITEM_COUNT.CARDS),
+  });
+};
