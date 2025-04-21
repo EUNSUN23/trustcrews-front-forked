445aa281dc67544c768c6d43537af953f22ef95d
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-04-22 01:46:50 +0900

    refactor: milestone,task 관련 컴포넌트 이동

---
+++ src/features/project/auth/myProject/jobs/components/task/Tasks.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { useState } from 'react';
+import TaskCard from '@/features/project/auth/myProject/jobs/components/task/taskList/TaskCard';
+import { DataId } from '@/utils/type';
+import CommonPagination from '@/components/ui/CommonPagination';
+import { ITEM_COUNT } from '@/utils/constant';
+import useTasks from '@/features/project/auth/myProject/jobs/service/task/getTaskList';
+import { useProjectManageAuth } from '@/features/project/auth/myProject/global/service/getProjectManageAuth';
+
+const Tasks = ({
+  projectId,
+  milestoneId,
+}: {
+  projectId: DataId;
+  milestoneId: bigint;
+}) => {
+  const {
+    data: { data: currentUserPMAuth },
+  } = useProjectManageAuth(projectId);
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const {
+    data: {
+      data: { content: taskList, totalPages },
+    },
+  } = useTasks({
+    projectId,
+    milestoneId,
+    pageNumber,
+    itemsPerPage: ITEM_COUNT.CARDS_SM,
+  });
+
+  function onChangePageHandler(pageNumber: number) {
+    setPageNumber(pageNumber - 1);
+  }
+
+  // if(isTasksLoading) return
+  //     <TasksSkeleton itemCount={ITEM_COUNT.CARDS_SM} />;
+
+  return (
+    <div className='w-full mt-4 flex flex-col items-center'>
+      {taskList.length > 0 ? (
+        <ul className='w-full grid pc:grid-cols-3 tablet:grid-cols-2 mobile:grid-cols-1 grid-rows-2 mobile:grid-rows-1 place-items-center gap-4 tablet:gap-8'>
+          {taskList.map((v) => (
+            <li key={v.workId}>
+              <TaskCard item={v} authMap={currentUserPMAuth!} />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className='w-full h-[14rem] flex items-center justify-center bg-ground200 rounded-lg'>
+          <span className='tablet:text-3xl text-grey800 font-semibold'>
+            업무를 추가해 주세요
+          </span>
+        </div>
+      )}
+      <CommonPagination
+        activePage={pageNumber + 1}
+        totalItemsCount={totalPages}
+        pageRangeDisplayed={5}
+        itemsCountPerPage={ITEM_COUNT.CARDS_SM}
+        onChangePageHandler={onChangePageHandler}
+      />
+    </div>
+  );
+};
+
+export default Tasks;
---
+++ src/features/project/auth/myProject/jobs/components/task/modal/TaskContent.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import Input from '@/components/ui/form/Input';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import {
+  TaskAddModalField,
+  taskModalDataFieldSelector,
+  taskModalEditDisabledSelector,
+  TaskModalType,
+  TaskModModalField,
+} from '@/features/project/auth/myProject/jobs/store/TaskModalStateStore';
+
+type TaskContentProps = {
+  modalType: TaskModalType;
+};
+
+function TaskContent({ modalType }: TaskContentProps) {
+  const disabled = useRecoilValue(taskModalEditDisabledSelector(modalType));
+  const [content, setContent] = useRecoilState(
+    taskModalDataFieldSelector({ modalType, fieldKey: 'content' }),
+  );
+
+  const value =
+    modalType === 'add'
+      ? (content as TaskAddModalField<'content'>)
+      : (content as TaskModModalField<'content'>);
+
+  return (
+    <div className='flex space-x-10 mobile:space-x-6'>
+      <label
+        htmlFor='content'
+        className='text-gray-700 font-semibold self-center'
+      >
+        제목
+      </label>
+      <div className='w-[250px] mobile:w-[220px]'>
+        <Input
+          id='content'
+          placeholder='제목 입력'
+          value={value}
+          onChange={(e) => setContent(e.target.value)}
+          maxLength={20}
+          disabled={disabled}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default TaskContent;
