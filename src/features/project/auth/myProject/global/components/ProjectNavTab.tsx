97aaf8205e634b80b55882f643b9911ad4cac36b
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-04-22 21:52:39 +0900

    fix: ProjectNavTab, MilestoneCardMenu, TaskCardMenu css 제대로 적용되도록 수정

--- src/features/project/auth/myProject/global/components/ProjectNavTab.tsx
+++ src/features/project/auth/myProject/global/components/ProjectNavTab.tsx
@@ -2,33 +2,26 @@
 
 import { useEffect } from 'react';
 import { useRecoilState, useResetRecoilState } from 'recoil';
-import { cva, VariantProps } from 'class-variance-authority';
-import { clsx } from 'clsx';
+import { cva } from 'class-variance-authority';
 import { PROJECT_MENU } from '@/features/project/auth/myProject/global/constants/projectMenu';
 import { projectActiveNavState } from '@/features/project/auth/myProject/global/store/ProjectNavTabStateStore';
 import { ProjectMenuKey } from '@/features/project/auth/myProject/global/types/projectMenu';
 
-const ProjectNavLinkVariants = cva({
-  variants: {
-    variant: {
-      default:
-        'border-transparent text-gray-500 hover:border-gray-200 hover:text-gray-700',
-      active: 'border-secondary text-secondary',
+const ProjectNavLinkVariants = cva(
+  'flex whitespace-nowrap border-b-[3px] -mb-[1.8px] py-4 px-1 mobile:px-4 pc:text-[2rem] tablet:text-[1.5rem] mobile:text-lg font-semibold',
+  {
+    variants: {
+      variant: {
+        default:
+          'border-transparent text-gray-500 hover:border-gray-200 hover:text-gray-700',
+        active: 'border-secondary text-secondary',
+      },
     },
   },
-  defaultVariants: 'default',
-});
+);
 
-type ProjectNavLinkVariantProps = VariantProps<typeof ProjectNavLinkVariants>;
-
-const projectNavLinkClass = (isActive: boolean): ProjectNavLinkVariantProps =>
-  clsx(
-    'flex whitespace-nowrap border-b-[3px] -mb-[1.8px] py-4 px-1 mobile:px-4 pc:text-[2rem] tablet:text-[1.5rem] mobile:text-lg font-semibold',
-    {
-      default: !isActive,
-      active: isActive,
-    },
-  );
+const projectNavLinkClass = (isActive: boolean) =>
+  ProjectNavLinkVariants({ variant: isActive ? 'active' : 'default' });
 
 const ProjectNavTab = () => {
   const [activeTab, setActiveTab] = useRecoilState(projectActiveNavState);
@@ -50,9 +43,7 @@ const ProjectNavTab = () => {
             {Object.values(PROJECT_MENU).map(({ name, value }) => (
               <li key={`tab-${value}`}>
                 <div
-                  className={ProjectNavLinkVariants(
-                    projectNavLinkClass(value === activeTab),
-                  )}
+                  className={projectNavLinkClass(value === activeTab)}
                   aria-current={value === activeTab}
                   onClick={() => handleClickNavTab(value)}
                 >
