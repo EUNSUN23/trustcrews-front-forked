0be3a3813f461fe1cde0135c282fca42244dd278
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-04-23 23:37:28 +0900

    refactor: PROJECT_MENU에 크루 상세 추가

--- src/features/project/auth/myProject/global/store/ProjectNavTabStateStore.tsx
+++ src/features/project/auth/myProject/global/store/ProjectNavTabStateStore.tsx
@@ -1,12 +1,12 @@
 import { atom } from 'recoil';
 import { PROJECT_MENU } from '@/features/project/auth/myProject/global/constants/projectMenu';
-import { ProjectMenuKey } from '@/features/project/auth/myProject/global/types/projectMenu';
+import { ProjectMenu } from '@/features/project/auth/myProject/global/types/projectMenu';
 
 const {
   TASK: { value: PROJECT_TASK },
 } = PROJECT_MENU;
 
-export const projectActiveNavState = atom<ProjectMenuKey>({
+export const projectActiveNavState = atom<ProjectMenu>({
   key: 'projectActiveNavState',
   default: PROJECT_TASK,
 });
