68497b735e2c929680fa275280eb7a06dadb513e
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-04-23 18:29:05 +0900

    refactor: crew관련 코드 스타일 통일

--- src/components/project/crews/detail/crewProfile/CrewProfile.tsx
+++ src/components/project/crews/detail/crewProfile/CrewProfile.tsx
@@ -4,37 +4,28 @@ import Avatar from '@/components/ui/Avatar';
 import PositionBadge from '@/components/ui/badge/PositionBadge';
 import ProjectRoleBadge from '@/components/ui/badge/ProjectRoleBadge';
 import CrewOutButton from '@/components/project/crews/detail/CrewOutButton';
-import ProfileSectionSkeleton from '@/components/ui/skeleton/project/crews/detail/ProfileSectionSkeleton';
-import useProjectMember from '@/hooks/project/crew/useProjectMember';
 import { getCookie } from 'cookies-next';
 import CrewFwButton from '@/components/project/crews/detail/CrewFWButton';
-import CrewProfileItem from '@/components/project/crews/detail/crewProfile/CrewProfileItem';
 import TechStackImage from '@/components/ui/TechStackImage';
 import { TechStackItem } from '@/utils/type';
 import TrustGradeBadge from '@/components/ui/badge/TrustGradeBadge';
+import { useCrewDetail } from '@/features/project/auth/myProject/crews/service/getCrewDetail';
 
-function CrewProfile({ projectMemberId }: { projectMemberId: string }) {
+type CrewProfileProps = {
+  projectMemberId: string;
+};
+
+const CrewProfile = ({ projectMemberId }: CrewProfileProps) => {
+  const {
+    data: { data: projectMemberInfo },
+  } = useCrewDetail(projectMemberId);
+
+  // if (isFetching) return <ProfileSectionSkeleton />;
+
+  const { user, position, projectMemberAuth } = projectMemberInfo;
+
   const currentUserId = getCookie('user_id');
-  const { projectMemberInfo, isFetching } = useProjectMember(projectMemberId);
-
-  if (isFetching) return <ProfileSectionSkeleton />;
-
-  const { user, position, projectMemberAuth } = projectMemberInfo!;
   const isMemberCurrentUser = currentUserId === user.userId.toString();
-
-  const techStacks = (
-    <ul className='flex items-center space-x-1'>
-      {user.technologyStacks.map((stack: TechStackItem) => (
-        <li key={stack.techStackId}>
-          <TechStackImage
-            stackName={stack.techStackName}
-            width={32}
-            height={32}
-          />
-        </li>
-      ))}
-    </ul>
-  );
 
   return (
     <div className='flex mobile:flex-col mobile:space-y-6 mobile:mt-4 px-1 py-4 mx-auto items-center justify-center'>
@@ -53,26 +44,55 @@ function CrewProfile({ projectMemberId }: { projectMemberId: string }) {
         )}
       </section>
       <section className='mobile:w-full tablet:h-[200px] mobile:h-[180px] flex flex-col flex-wrap justify-between p-6 mobile:p-4 bg-ground100 rounded-lg'>
-        <CrewProfileItem
-          label='프로젝트 권한'
-          contents={<ProjectRoleBadge projectAuth={projectMemberAuth} />}
-        />
-        <CrewProfileItem
-          label='프로젝트 포지션'
-          contents={<PositionBadge text={position.name} size='sm' />}
-        />
-        <CrewProfileItem label='기술스택' contents={techStacks} />
-        <CrewProfileItem
-          label='신뢰점수'
-          contents={
+        <div className='pc:h-[50px] tablet:mx-8 flex items-center justify-around mobile:space-x-4'>
+          <span className='tablet:w-[200px] tablet:text-[1.2rem] font-medium text-geryDarkBlue'>
+            프로젝트 권한
+          </span>
+          <div className='min-w-[100px] flex justify-center grow-0 mx-auto'>
+            <ProjectRoleBadge auth={projectMemberAuth.code}>
+              {projectMemberAuth.name}
+            </ProjectRoleBadge>
+          </div>
+        </div>
+        <div className='pc:h-[50px] tablet:mx-8 flex items-center justify-around mobile:space-x-4'>
+          <span className='tablet:w-[200px] tablet:text-[1.2rem] font-medium text-geryDarkBlue'>
+            프로젝트 포지션
+          </span>
+          <div className='min-w-[100px] flex justify-center grow-0 mx-auto'>
+            <PositionBadge text={position.name} size='sm' />
+          </div>
+        </div>
+        <div className='pc:h-[50px] tablet:mx-8 flex items-center justify-around mobile:space-x-4'>
+          <span className='tablet:w-[200px] tablet:text-[1.2rem] font-medium text-geryDarkBlue'>
+            기술스택
+          </span>
+          <div className='min-w-[100px] flex justify-center grow-0 mx-auto'>
+            <ul className='flex items-center space-x-1'>
+              {user.technologyStacks.map((stack: TechStackItem) => (
+                <li key={stack.techStackId}>
+                  <TechStackImage
+                    stackName={stack.techStackName}
+                    width={32}
+                    height={32}
+                  />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className='pc:h-[50px] tablet:mx-8 flex items-center justify-around mobile:space-x-4'>
+          <span className='tablet:w-[200px] tablet:text-[1.2rem] font-medium text-geryDarkBlue'>
+            신뢰점수
+          </span>
+          <div className='min-w-[100px] flex justify-center grow-0 mx-auto'>
             <span className='pc:text-lg tablet:text-base mobile:text-sm font-semibold'>
               {user.trustScore} 점
             </span>
-          }
-        />
+          </div>
+        </div>
       </section>
     </div>
   );
-}
+};
 
 export default CrewProfile;
