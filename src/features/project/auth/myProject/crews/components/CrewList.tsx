f7fcc1d735328cf6b8a1c4ba4d7039fd02b561bc
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-04-23 23:40:55 +0900

    refactor: 크루목록 클릭시 crewIdState 초기화하고 activeTab 업데이트하도록 수정

--- src/features/project/auth/myProject/crews/components/CrewList.tsx
+++ src/features/project/auth/myProject/crews/components/CrewList.tsx
@@ -2,20 +2,41 @@
 
 import Avatar from '@/components/ui/Avatar';
 import ProjectRoleBadge from '@/components/ui/badge/ProjectRoleBadge';
-import Link from 'next/link';
 import PositionBadge from '@/components/ui/badge/PositionBadge';
 import { ProjectCrew } from '@/features/project/auth/myProject/crews/types';
 import { useProjectCrewList } from '@/features/project/auth/myProject/crews/service/getProjectCrewList';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { projectIdState } from '@/features/project/auth/myProject/global/store/ProjectIdStateStore';
+import { crewIdState } from '@/features/project/auth/myProject/crews/store/CrewIdStateStore';
+import { projectActiveNavState } from '@/features/project/auth/myProject/global/store/ProjectNavTabStateStore';
+import { useTransition } from 'react';
+import { PROJECT_MENU } from '@/features/project/auth/myProject/global/constants/projectMenu';
+
+const {
+  CREWS: {
+    child: {
+      CREW_DETAIL: { value: PROJECT_CREW_DETAIL },
+    },
+  },
+} = PROJECT_MENU;
 
 const CrewList = () => {
+  const setActiveNavTab = useSetRecoilState(projectActiveNavState);
+  const [_, startTransition] = useTransition();
+  const setCrewIdState = useSetRecoilState(crewIdState);
   const projectId = useRecoilValue(projectIdState);
   const {
     data: {
       data: { projectMembers: crewList },
     },
   } = useProjectCrewList(projectId);
+
+  const handleClickCrew = (projectMemberId: bigint) => {
+    startTransition(() => {
+      setActiveNavTab(PROJECT_CREW_DETAIL);
+    });
+    setCrewIdState(projectMemberId);
+  };
 
   return (
     <ul role='list' className='min-h-[350px]'>
@@ -29,18 +50,10 @@ const CrewList = () => {
           return (
             <li
               key={projectMemberUseId}
-              className='flex items-center gap-x-6 py-5 cursor-pointer hover:bg-grey000 border-b border-grey300'
+              className='cursor flex items-center gap-x-6 py-5 cursor-pointer hover:bg-grey000 border-b border-grey300'
+              onClick={() => handleClickCrew(projectMemberId)}
             >
-              <Link
-                href={{
-                  pathname: `/projectCrewDetail`,
-                  query: {
-                    projectId,
-                    projectMemberId: projectMemberId.toString(),
-                  },
-                }}
-                className='w-full min-w-0 flex mobile:flex-col items-center mobile:items-start mobile:space-y-3 tablet:px-6 mobile:pl-4'
-              >
+              <div className='w-full min-w-0 flex mobile:flex-col items-center mobile:items-start mobile:space-y-3 tablet:px-6 mobile:pl-4'>
                 <div className='min-w-0 flex items-center tablet:space-x-6 mobile:space-x-4'>
                   <Avatar
                     size='xs'
@@ -61,7 +74,7 @@ const CrewList = () => {
                     </li>
                   </ul>
                 </div>
-              </Link>
+              </div>
             </li>
           );
         },
