811b598de3fc97b41782f27c84541e6e9d2d09c6
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-04-24 23:27:21 +0900

    fix: ProjectManageAuthState 기본값 code타입 ProjectAuthCode로 수정

--- src/features/project/auth/myProject/global/store/ProjectManageAuthStateStore.tsx
+++ src/features/project/auth/myProject/global/store/ProjectManageAuthStateStore.tsx
@@ -1,9 +1,11 @@
 import { atom } from 'recoil';
 import { ProjectAuthMap } from '@/features/project/auth/myProject/global/types/projectAuth';
+import { PROJECT_AUTH_CODE } from '@/features/project/auth/myProject/global/constants/projectAuthCode';
 
-const DEFAULT_PM_AUTH = {
-  code: 'PAUTH_2001',
-  name: '크루',
+const { CREW: CREW_AUTH } = PROJECT_AUTH_CODE;
+export const DEFAULT_PM_AUTH = {
+  code: CREW_AUTH,
+  name: '',
   workChangeYN: false,
   milestoneChangeYN: false,
   configYn: false,
