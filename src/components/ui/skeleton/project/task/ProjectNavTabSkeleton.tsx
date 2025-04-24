11e8087c897f08e5cd9a2a36d748dd3d5f9f044f
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-04-25 02:24:36 +0900

    fix: ProjectNavTab css 수정 (cursor-pointer, skeleton)

--- src/components/ui/skeleton/project/task/ProjectNavTabSkeleton.tsx
+++ src/components/ui/skeleton/project/task/ProjectNavTabSkeleton.tsx
@@ -7,15 +7,15 @@ import { PROJECT_MENU } from '@/features/project/auth/myProject/global/constants
 function ProjectNavTabSkeleton() {
   return (
     <div className='tablet:my-[3.9rem] mobile:my-[3rem]'>
-      <div className='border-b-[3px] border-grey300'>
+      <div className='border-b-[3px] border-grey300 pb-6'>
         <nav
-          className='-mb-px flex tablet:space-x-10 mobile:justify-between'
+          className='-mb-px flex tablet:space-x-10 mobile:justify-around'
           aria-label='Tabs'
         >
           {Object.values(PROJECT_MENU).map((v) => (
             <Skeleton
               key={v.name}
-              sizeClassName='w-[50px] h-[45px] -mb-[1.8px] py-4 px-1 mobile:px-4'
+              sizeClassName='w-[90px] h-[45px] mobile:w-[60px] mobile:h-[40px] -mb-[1.8px] py-4 px-1 mobile:px-4'
             />
           ))}
         </nav>
