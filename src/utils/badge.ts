64159e4732a4b595d31d930afcb9688fffdb9448
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-04-25 02:11:12 +0900

    fix: 뱃지 base lg사이즈 수정

--- src/utils/badge.ts
+++ src/utils/badge.ts
@@ -2,7 +2,7 @@ export const baseBadgeVariants = {
   size: {
     xs: 'text-[11px] mobile:text-[9px] px-2 mobile:px-1 py-0.5 mobile:py-0',
     sm: 'tablet:text-sm mobile:text-xs px-2 py-1',
-    md: 'tablet:text-lg mobile:text-base tablet:px-8 mobile:px-4 tablet:py-4 mobile:py-2',
+    md: 'tablet:text-lg mobile:text-sm tablet:px-4 mobile:px-2 tablet:py-1.5 mobile:py-[5px]',
     lg: 'text-lg px-8 py-4',
   },
 };
