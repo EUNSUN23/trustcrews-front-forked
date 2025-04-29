86a6e19a64b358f09e5b94705503bb40dce380b1
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-04-29 22:40:00 +0900

    refactor: notice 관련 store, service, constants, types 이동

---
+++ src/features/project/auth/myProject/notice/service/getFWVoteNoticeList.ts
@@ -0,0 +1,38 @@
+import { request } from '@/lib/clientApi/request';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { bigIntToString } from '@/utils/common';
+import { ITEM_COUNT } from '@/utils/constant';
+import { NOTICE_TYPES } from '@/features/project/auth/myProject/notice/constants/noticeTypes';
+import { VoteStatusType } from '@/features/project/auth/myProject/vote/types';
+import { PageResponseBody } from '@/utils/type';
+
+export type FWVoteNoticeData = {
+  alertId: bigint;
+  voteId: bigint;
+  fwMemberId: bigint;
+  alertType: typeof NOTICE_TYPES.PRA1003;
+  contents: string;
+  voteStatus: VoteStatusType;
+  createDate: string;
+};
+
+export const getFWVoteNoticeList = async (
+  projectId: bigint,
+  pageIndex: number,
+  itemCount: number,
+): Promise<PageResponseBody<FWVoteNoticeData[]>> => {
+  return request(
+    'GET',
+    `/api/project/alert/vote/fwithdraw?projectId=${projectId}&pageIndex=${pageIndex}&itemCount=${itemCount}`,
+  );
+};
+
+export const useFWVoteNoticeList = (projectId: bigint, pageIndex: number) => {
+  return useSuspenseQuery({
+    queryKey: ['fwVoteNoticeList', bigIntToString(projectId), pageIndex],
+    queryFn: () =>
+      getFWVoteNoticeList(projectId, pageIndex, ITEM_COUNT.LIST_SM),
+    refetchInterval: 60000,
+    refetchIntervalInBackground: true,
+  });
+};
