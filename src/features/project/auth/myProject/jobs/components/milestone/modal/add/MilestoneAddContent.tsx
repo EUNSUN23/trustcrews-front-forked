ad4afe11b91db7b1cbdfdd6ce0558b7af74ffe75
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-04-29 18:01:42 +0900

    refactor: milestone 리팩토링 보완

--- src/features/project/auth/myProject/jobs/components/milestone/modal/add/MilestoneAddContent.tsx
+++ src/features/project/auth/myProject/jobs/components/milestone/modal/add/MilestoneAddContent.tsx
@@ -13,14 +13,14 @@ const MilestoneAddContent = () => {
   return (
     <div className='flex'>
       <label
-        htmlFor='content'
+        htmlFor='milestoneAddContent'
         className='text-gray-700 font-semibold self-center'
       >
         내용
       </label>
       <div className='w-[350px] mobile:w-[220px] ml-auto'>
         <Input
-          id='content'
+          id='milestoneAddContent'
           placeholder='내용을 입력해주세요.'
           value={content as MilestoneAddDataField<'content'>}
           onChange={(e) => setContent(e.target.value)}
