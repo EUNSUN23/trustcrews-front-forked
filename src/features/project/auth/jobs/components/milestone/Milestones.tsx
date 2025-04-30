438ba798339a655daced294f1b2ed31eac979c50
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-04-30 22:16:38 +0900

    refactor: project/auth/myProject 이하 feature들 project/auth 이하로 이동

---
+++ src/features/project/auth/jobs/components/milestone/Milestones.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import MilestoneCard from '@/features/project/auth/jobs/components/milestone/card/MilestoneCard';
+import CustomSwiper from '@/components/ui/CustomSwiper';
+import { MilestoneInfo } from '@/features/project/auth/jobs/types/milestone';
+
+type MilestonesProps = {
+  data: MilestoneInfo[];
+  totalCounts: number;
+};
+
+const Milestones = ({ data, totalCounts }: MilestonesProps) => {
+  if (totalCounts === 0)
+    return (
+      <div className='w-full h-[12rem] flex items-center justify-center bg-ground200 rounded-lg'>
+        <span className='tablet:text-3xl text-grey800 font-semibold'>
+          마일스톤을 추가해 주세요
+        </span>
+      </div>
+    );
+
+  return (
+    <CustomSwiper
+      slideItems={data.map((v) => ({
+        key: v.milestoneId.toString(),
+        components: <MilestoneCard milestoneInfo={v} />,
+      }))}
+    />
+  );
+};
+
+export default Milestones;
---
+++ src/features/project/auth/jobs/service/milestone/getMilestones.ts
@@ -0,0 +1,33 @@
+import { request } from '@/lib/clientApi/request';
+import { sortByStartDate } from '@/utils/common';
+import { ResponseBody } from '@/utils/type';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { MilestoneInfo } from '@/features/project/auth/jobs/types/milestone';
+
+export const getMilestones = async (
+  projectId: string,
+): Promise<ResponseBody<MilestoneInfo[]>> => {
+  const resBody = await request(
+    'GET',
+    `/api/project/milestone?projectId=${projectId}`,
+  );
+
+  return {
+    ...resBody,
+    data: resBody.data
+      ? sortByStartDate(resBody.data!, 'asc').map((v, index) => ({
+          ...v,
+          index,
+        }))
+      : [],
+  };
+};
+
+export const getMilestonesQueryKey = ['milestoneList'];
+
+export const useMilestones = (projectId: string) => {
+  return useSuspenseQuery({
+    queryKey: [...getMilestonesQueryKey, projectId],
+    queryFn: () => getMilestones(projectId),
+  });
+};
