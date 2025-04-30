e14c467df5c632c742932c550b2f7b764850b76d
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-04-30 21:25:55 +0900

    fix: notice탭 기본값 모집 탭으로 변경

--- src/features/project/auth/myProject/notice/store/ActiveNoticeTabStateStore.tsx
+++ src/features/project/auth/myProject/notice/store/ActiveNoticeTabStateStore.tsx
@@ -2,9 +2,9 @@ import { atom } from 'recoil';
 import { NoticeTab } from '@/features/project/auth/myProject/notice/types';
 import { NOTICE_TABS } from '@/features/project/auth/myProject/notice/constants/noticeTabs';
 
-const { NTAB003: CREW_NOTICE_TAB } = NOTICE_TABS;
+const { NTAB001: RCVOTE_NOTICE_TAB } = NOTICE_TABS;
 
 export const activeNoticeTabStateStore = atom<NoticeTab>({
   key: 'activeNoticeTabStateStore',
-  default: CREW_NOTICE_TAB,
+  default: RCVOTE_NOTICE_TAB,
 });
