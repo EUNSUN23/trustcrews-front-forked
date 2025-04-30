36be71e7c82788f8bd150e4e79a75e44af536d56
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-04-30 21:23:02 +0900

    fix: ProjectManageAuthStateStore 기본auth 크루권한에서 빈값으로 수정

--- src/features/project/auth/myProject/global/store/ProjectManageAuthStateStore.tsx
+++ src/features/project/auth/myProject/global/store/ProjectManageAuthStateStore.tsx
@@ -1,17 +1,19 @@
 import { atom } from 'recoil';
 import { ProjectAuthMap } from '@/features/project/auth/myProject/global/types/projectAuth';
-import { PROJECT_AUTH_CODE } from '@/features/project/auth/myProject/global/constants/projectAuthCode';
 
-const { CREW: CREW_AUTH } = PROJECT_AUTH_CODE;
+interface ProjectManageAuthState extends Omit<ProjectAuthMap, 'code'> {
+  code: string;
+}
+
 export const DEFAULT_PM_AUTH = {
-  code: CREW_AUTH,
+  code: '',
   name: '',
   workChangeYN: false,
   milestoneChangeYN: false,
   configYn: false,
 } as const;
 
-export const projectManageAuthStateStore = atom<ProjectAuthMap>({
+export const projectManageAuthStateStore = atom<ProjectManageAuthState>({
   key: 'projectManageAuthStateStore',
   default: DEFAULT_PM_AUTH,
 });
