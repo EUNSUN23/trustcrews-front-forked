b2cd77f0460a028913865361afdbfbd398049edb
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-05-02 02:20:07 +0900

    refactor: setting 라우트 제거 & ProjectConfig 연결

--- src/app/project/[slug]/setting/_utils/type.ts
+++
@@ -1,6 +0,0 @@
-import { TRUST_GRADE } from '@/app/project/@setting/_utils/constant';
-import { ArrayValue } from '@/utils/type';
-
-export type TrustGradeSelectType = ArrayValue<typeof TRUST_GRADE.values>;
-export type TrustGradeNameType = TrustGradeSelectType['name'];
-export type TrustGradeValueType = TrustGradeSelectType['value'];
