3bcf5e606311bdcc952ebc1b991832a14c0ebaf4
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-05-02 00:59:40 +0900

    refactor: 프로젝트 정보, 프로젝트 게시물 정보 수정 feature 분리

---
+++ src/features/project/auth/updateProjectInfo/contents/ProjectInfoForm.tsx
@@ -0,0 +1,42 @@
+import ProjectName from '@/features/project/auth/updateProjectInfo/components/ProjectName';
+import ProjectSubject from '@/features/project/auth/updateProjectInfo/components/ProjectSubject';
+import ProjectDate from '@/features/project/auth/updateProjectInfo/components/ProjectDate';
+import ProjectInfoResetButton from '@/features/project/auth/updateProjectInfo/contents/ProjectInfoResetButton';
+import ProjectInfoSaveButton from '@/features/project/auth/updateProjectInfo/contents/ProjectInfoSaveButton';
+import { useRecoilValue } from 'recoil';
+import ProjectTechnologies from '@/features/project/auth/updateProjectInfo/components/ProjectTechnologies';
+import ConfigContainer from '@/features/project/auth/global/components/projectConfigs/ConfigContainer';
+import ConfigSummary from '@/features/project/auth/global/components/projectConfigs/ConfigSummary';
+import ConfigContents from '@/features/project/auth/global/components/projectConfigs/ConfigContents';
+import { numStrToBigInt } from '@/utils/common';
+import { useProjectPublicInfo } from '@/features/project/public/service/getProjectPublicInfo';
+import { projectIdState } from '@/features/project/auth/global/store/ProjectIdStateStore';
+
+const ProjectInfoForm = () => {
+  const projectId = useRecoilValue(projectIdState);
+
+  const {
+    data: { data: projectInfo },
+  } = useProjectPublicInfo(numStrToBigInt(projectId));
+
+  const { projectName, projectSubject, startDate, endDate, technologyStacks } =
+    projectInfo;
+
+  return (
+    <ConfigContainer>
+      <ConfigSummary>프로젝트 정보</ConfigSummary>
+      <ConfigContents>
+        <ProjectName initData={projectName} />
+        <ProjectSubject initData={projectSubject} />
+        <ProjectDate initStartDate={startDate} initEndDate={endDate} />
+        <ProjectTechnologies initData={technologyStacks} />
+      </ConfigContents>
+      <div className='w-full my-4 flex items-center justify-center space-x-2'>
+        <ProjectInfoResetButton />
+        <ProjectInfoSaveButton initData={projectInfo} />
+      </div>
+    </ConfigContainer>
+  );
+};
+
+export default ProjectInfoForm;
