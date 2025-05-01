d2393aea5ca7a14d2fa5a44d3e6fe13ce598d6d8
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-05-02 02:19:35 +0900

    refactor: 프로젝트 종료기능 feature 분리

---
+++ src/features/project/auth/endProject/service/endProject.ts
@@ -0,0 +1,42 @@
+import { request } from '@/lib/clientApi/request';
+import { ApiResult, ResponseBody } from '@/utils/type';
+import { useMutation } from '@tanstack/react-query';
+
+export const endProject = async (
+  projectId: bigint,
+): Promise<ResponseBody<null>> => {
+  return await request('POST', '/api/project', { projectId });
+};
+
+type EndProjectRes = ApiResult<typeof endProject>;
+
+// todo - 백엔드 성공 메세지
+export const useEndProject = (
+  projectId: bigint,
+  {
+    onSuccess,
+    onError,
+  }: {
+    onSuccess?: (res: EndProjectRes) => void;
+    onError?: (res: EndProjectRes) => void;
+  },
+) => {
+  return useMutation({
+    mutationFn: () => endProject(projectId),
+    onSuccess: (res) => {
+      if (res.result === 'success') {
+        onSuccess?.(res);
+      } else {
+        onError?.(res);
+      }
+    },
+    onError: (error) => {
+      console.error(error.cause);
+      onError?.({
+        result: 'fail',
+        data: null,
+        message: '프로젝트 종료 중 오류가 발생했습니다.',
+      });
+    },
+  });
+};
