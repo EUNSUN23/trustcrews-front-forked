4e7d7a3514cb9d12d679c24570766bce7f3eb465
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-05-02 20:34:45 +0900

    refactor: jobs contents 분리

---
+++ src/features/project/auth/projectJobs/components/form/milestone/MilestoneAddContent.tsx
@@ -0,0 +1,37 @@
+import Input from '@/components/ui/form/Input';
+import { useRecoilState } from 'recoil';
+import { milestoneAddFormFieldSelector } from '@/features/project/auth/projectJobs/store/MilestoneModalStateStore';
+import { ChangeEvent } from 'react';
+
+const INPUT_ID = 'milestoneAddContent';
+
+const MilestoneAddContent = () => {
+  const [content, setContent] = useRecoilState(
+    milestoneAddFormFieldSelector('content'),
+  );
+
+  const handleChangeContent = (e: ChangeEvent<HTMLInputElement>) => {
+    setContent(e.target.value);
+  };
+
+  return (
+    <div className='flex'>
+      <label
+        htmlFor={INPUT_ID}
+        className='text-gray-700 font-semibold self-center'
+      >
+        내용
+      </label>
+      <div className='w-[350px] mobile:w-[220px] ml-auto'>
+        <Input
+          id={INPUT_ID}
+          placeholder='내용을 입력해주세요.'
+          value={content}
+          onChange={handleChangeContent}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default MilestoneAddContent;
--- src/features/project/auth/projectJobs/types/task.ts
+++ src/features/project/auth/projectJobs/types/task.ts
@@ -2,25 +2,4 @@ import { TASK_STATUS } from '@/features/project/auth/projectJobs/constants/task/
 
 export type TaskStatusKey = keyof typeof TASK_STATUS;
 export type TaskStatus = (typeof TASK_STATUS)[TaskStatusKey];
-export type TaskStatusName = (typeof TASK_STATUS)[TaskStatusKey]['name'];
 export type TaskStatusCode = (typeof TASK_STATUS)[TaskStatusKey]['code'];
-
-export type TaskItem = {
-  workId: bigint;
-  projectId: bigint;
-  milestoneId: bigint;
-  assignedUser: AssignedUser | null;
-  lastModifiedMemberNickname: string;
-  content: string;
-  startDate: string;
-  endDate: string;
-  progressStatus: TaskStatus;
-  contentDetail: string | '';
-};
-
-export type TaskContentDetails = Map<string, string>;
-
-export type AssignedUser = {
-  projectMemberId: bigint;
-  nickname: string;
-};
