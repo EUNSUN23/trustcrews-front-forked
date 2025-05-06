c7a8c5e1a3679282d3f11670a34aaf1bdfe001fb
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-05-06 19:20:28 +0900

    refactor: 공통 form button cn적용

--- src/components/ui/form/FormButton.tsx
+++ src/components/ui/form/FormButton.tsx
@@ -1,18 +1,22 @@
 import { ButtonHTMLAttributes, ReactNode } from 'react';
+import cn from '@/utils/cn';
 
 interface FormButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children?: ReactNode;
 }
 
-function FormButton({ children, ...props }: FormButtonProps) {
+const FormButton = ({ children, ...props }: FormButtonProps) => {
   return (
     <button
-      className='rounded-full w-full h-12 mobile:h-10 py-2 px-4 font-medium bg-primary text-white shadow-sm mobile:text-sm'
+      className={cn(
+        'rounded-full w-full h-12 mobile:h-10 py-2 px-4 font-medium bg-primary text-white shadow-sm mobile:text-sm',
+        props.className,
+      )}
       {...props}
     >
       {children}
     </button>
   );
-}
+};
 
 export default FormButton;
