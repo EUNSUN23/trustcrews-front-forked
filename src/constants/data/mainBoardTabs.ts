ba825add21cdb08ba4272ae4e92a56cb6012e76c
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-05-09 23:39:03 +0900

    refactor: BoardActiveStateStore -> ActiveMainBoardTabStateStore로 변경하고 전역 store로 이동

---
+++ src/constants/data/mainBoardTabs.ts
@@ -0,0 +1,6 @@
+const MAIN_BOARD_TABS = {
+  BM_TAB001: { desc: '내 프로젝트 관리', code: 'BM_TAB001' },
+  BM_TAB002: { desc: '팀 프로젝트', code: 'BM_TAB002' },
+} as const;
+
+export default MAIN_BOARD_TABS;
