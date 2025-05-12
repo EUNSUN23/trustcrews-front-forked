4f8da0f6ddce309ee869a694b3ecb09cdf47480c
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-05-12 19:42:19 +0900

    feat: useSuspenseQuery용 Suspense + ErrorBoundary 추가

--- src/utils/clientApi/handleResponse.ts
+++ src/utils/clientApi/handleResponse.ts
@@ -1,21 +1,37 @@
-import { ResponseBody } from '@/types/responseBody';
+import { HttpError } from '@/utils/clientApi/HttpError';
 
 const handleResponse = async (res: Response) => {
-  if (res.ok) return res.json();
+  if (res.ok) {
+    return res.json();
+  } else {
+    const body = await res.json().catch(() => null);
 
-  const data: ResponseBody<null> = await res.json();
-  const errorInstruction = res.headers.get('X-Error-Instruction');
-
-  if (!errorInstruction || errorInstruction === 'NONE') {
-    throw new Error();
+    throw new HttpError(
+      res.status,
+      body || {
+        result: 'fail',
+        data: null,
+        message: '요청 수행 중 오류가 발생했습니다.',
+      },
+      res.statusText,
+    );
   }
 
-  if (errorInstruction === 'REDIRECT') {
-    const errorRoute = `/error/${res.status}?error=${data.message}`;
-    window.location.assign(errorRoute);
-  } else if (errorInstruction === 'MESSAGE') {
-    return data;
-  }
+  // todo - 메세지 연결
+  // console.log('res in handle: ', res);
+  // const data: ResponseBody<null> = await res.json();
+  // const errorInstruction = res.headers.get('X-Error-Instruction');
+  //
+  // if (!errorInstruction || errorInstruction === 'NONE') {
+  //   throw new Error();
+  // }
+  //
+  // if (errorInstruction === 'REDIRECT') {
+  //   const errorRoute = `/error/${res.status}?error=${data.message}`;
+  //   window.location.assign(errorRoute);
+  // } else if (errorInstruction === 'MESSAGE') {
+  //   return data;
+  // }
 };
 
 export default handleResponse;
