8f6b1bdd63aae60a6e1fb5ecd00eed53df503afb
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-05-12 20:31:39 +0900

    feat: MultiPositionSelect 관련 Suspense + ErrorBoundary 적용

--- src/features/projectConfig/private/components/postInfo/BoardPositions.tsx
+++ src/features/projectConfig/private/components/postInfo/BoardPositions.tsx
@@ -2,6 +2,9 @@ import { useRecoilState } from 'recoil';
 import MultiPositionSelect from '@/components/position/public/MultiPositionSelect';
 import { PositionId } from '@/types/data/position';
 import { postInfoFormFieldSelector } from '@/features/projectConfig/private/store/PostInfoFormStateStore';
+import SelectSkeleton from '@/shared/ui/skeleton/SelectSkeleton';
+import FieldQueryBoundary from '@/components/error/FieldQueryBoundary';
+import { Field, Label } from '@headlessui/react';
 
 type BoardPositionsProps = {
   initData: PositionId[];
@@ -15,10 +18,19 @@ const BoardPositions = ({ initData }: BoardPositionsProps) => {
   const value = positionsId.length > 0 ? positionsId : initData;
 
   return (
-    <MultiPositionSelect
-      positions={value}
-      setPositions={(item) => setPositionsId(item)}
-    />
+    <Field>
+      <Label className='block text-gray-700 mobile:text-sm'>모집 분야</Label>
+      <FieldQueryBoundary
+        suspenseFallback={
+          <SelectSkeleton placeholder='모집 분야를 선택해주세요.' />
+        }
+      >
+        <MultiPositionSelect
+          positions={value}
+          setPositions={(item) => setPositionsId(item)}
+        />
+      </FieldQueryBoundary>
+    </Field>
   );
 };
 
