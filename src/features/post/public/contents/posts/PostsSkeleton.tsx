98e8ff545a18b9625ebbae22d92f07228893608b
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-05-13 23:13:17 +0900

    feat: 게시글조회 suspense, errorBoundary 적용

--- src/features/post/public/contents/posts/PostsSkeleton.tsx
+++ src/features/post/public/contents/posts/PostsSkeleton.tsx
@@ -1,7 +1,7 @@
 import TechStackFilterSkeleton from '@/features/post/public/contents/postFilter/TeckStackFilterSkeleton';
-import { PositionFilterSkeleton } from '@/features/post/public/contents/postFilter/PositionFilterSkeleton';
 import CardListSkeleton from '@/shared/ui/skeleton/CardListSkeleton';
 import Skeleton from '@/shared/ui/Skeleton';
+import PositionFilterSkeleton from '@/features/post/public/contents/postFilter/PositionFilterSkeleton';
 
 const PostsSkeleton = () => {
   return (
@@ -9,15 +9,17 @@ const PostsSkeleton = () => {
       <h2 className='sr-only'>팀 프로젝트</h2>
       <section
         aria-label='게시글 검색'
-        className='mt-6 flex justify-between mobile:block mobile:space-y-5'
+        className='mt-6 flex  mobile:block mobile:space-y-5'
       >
-        <div className='flex space-x-5'>
+        <div className='flex justify-start space-x-5 mr-auto'>
           <TechStackFilterSkeleton />
           <PositionFilterSkeleton />
         </div>
-        <Skeleton className='w-[150px] h-[40px]' />
+        <Skeleton className='w-[230px] h-[40px]' />
       </section>
-      <CardListSkeleton itemCount={8} />
+      <section className='mt-6 mobile:mt-2'>
+        <CardListSkeleton itemCount={8} />
+      </section>
     </section>
   );
 };
