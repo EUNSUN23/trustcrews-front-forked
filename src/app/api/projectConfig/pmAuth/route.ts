9afbc2e80440a19bd551c6d0152f2fb576071ae7
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-05-13 16:31:32 +0900

    feat: api/project/setting -> api/project/projectConfig로 수정

--- src/app/api/projectConfig/pmAuth/route.ts
+++ src/app/api/projectConfig/pmAuth/route.ts
@@ -1,18 +1,13 @@
 import { NextRequest } from 'next/server';
-import { routeResponse } from '@/app/api/_interceptor/routeResponse';
 import authApi from '@/app/api/_interceptor/authApi';
 import { JSONReplaceBigInt } from '@/shared/utils/jsonUtils';
+import { routeResponse } from '@/app/api/_interceptor/routeResponse';
 
-/**
- * 프로젝트 설정 - 크루 권한 업데이트
- * @param req
- * @constructor
- */
 export async function PUT(req: NextRequest) {
   const reqData = await req.json();
   const method = req.method;
 
-  const res = await authApi('/api/project/setting/crewAuth', {
+  const res = await authApi('/api/projectConfig/pmAuth', {
     method,
     body: JSONReplaceBigInt(reqData),
   });
