b3095661a2565efa397d399f2c44f8c98b6ec5ab
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-05-14 18:05:19 +0900

    feat: 프로젝트 크루 관련 코드 Suspense & ErrorBoundary 적용

--- src/features/projectCrews/private/service/getProjectCrewList.ts
+++ src/features/projectCrews/private/service/getProjectCrewList.ts
@@ -2,20 +2,19 @@ import { request } from '@/utils/clientApi/request';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { ResponseBody } from '@/types/responseBody';
 import { ProjectCrew } from '@/features/projectCrews/private/types';
+import { bigIntToString } from '@/shared/utils/stringUtils';
 
-export const getProjectCrewList = async ({
-  projectId,
-}: {
-  projectId: string | bigint;
-}): Promise<ResponseBody<Record<'projectMembers', ProjectCrew[]>>> => {
+export const getProjectCrewList = async (
+  projectId: bigint,
+): Promise<ResponseBody<Record<'projectMembers', ProjectCrew[]>>> => {
   return await request('GET', `/api/project/crews/list?projectId=${projectId}`);
 };
 
 export const CREW_LIST_QUERY_KEY = 'crewList';
 
-export const useProjectCrewList = (projectId: string) => {
+export const useProjectCrewList = (projectId: bigint) => {
   return useSuspenseQuery({
-    queryKey: [CREW_LIST_QUERY_KEY, projectId],
-    queryFn: () => getProjectCrewList({ projectId }),
+    queryKey: [CREW_LIST_QUERY_KEY, bigIntToString(projectId)],
+    queryFn: () => getProjectCrewList(projectId),
   });
 };
