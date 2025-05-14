950ae7479b8d447c88dfbf1cb046c6e43db9e7ed
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-05-14 18:05:19 +0900

    feat: 프로젝트 크루 관련 코드 Suspense & ErrorBoundary 적용

--- src/constants/defaultSelectOptions.ts
+++ src/constants/defaultSelectOptions.ts
@@ -1,3 +1,3 @@
 export const DEFAULT_POSITION_OPTION = { name: '포지션', value: 0n } as const;
 
-export const DEFAULT_CREW_OPTION = { name: '멤버 선택', value: '0' } as const;
+export const DEFAULT_CREW_OPTION = { name: '크루 선택', value: '0' } as const;
