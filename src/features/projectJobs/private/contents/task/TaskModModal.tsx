53ac9c86362e7e90fb591c08ff921a8a4fe58e6e
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-05-15 20:12:03 +0900

    refactor: 미사용 코드 제거

--- src/features/projectJobs/private/contents/task/TaskModModal.tsx
+++ src/features/projectJobs/private/contents/task/TaskModModal.tsx
@@ -14,19 +14,13 @@ import {
 import useUpdateTask, {
   updateTaskInputSchema,
 } from '@/features/projectJobs/private/service/task/updateTask';
-import { useCompleteTask } from '@/features/projectJobs/private/service/task/completeTask';
 import useSnackbar from '@/shared/hooks/useSnackbar';
 import { ZodError } from 'zod';
-import { TASK_STATUS } from '@/features/projectJobs/private/constants/task/taskStatus';
 import TaskTodoControl from '@/features/projectJobs/private/components/task/taskTodoControl';
 import { numStrToBigInt } from '@/shared/utils/stringUtils';
 import TaskProgressStatusControl from '@/features/projectJobs/private/components/task/form/TaskProgressStatusControl';
 
 const modalType: TaskModalType = 'mod';
-
-const {
-  PS003: { code: TASK_COMPLETE },
-} = TASK_STATUS;
 
 const TaskModModal = () => {
   const resetTaskModModalState = useResetRecoilState(taskModModalStateStore);
@@ -52,47 +46,24 @@ const TaskModModal = () => {
     },
   );
 
-  const { completeTask, isUpdating: isCompleting } = useCompleteTask(
-    numStrToBigInt(workId),
-    auth,
-    {
-      onSuccess: (res) => {
-        resetTaskModModalState();
-        resetTaskModModalData();
-        setSuccessSnackbar(res.message);
-      },
-      onError: (error) => setErrorSnackbar(error.message),
-    },
-  );
-
   const handleClickCloseButton = () => {
     resetTaskModModalState();
     resetTaskModModalData();
   };
 
   const handleClickConfirmButton = () => {
-    if (modModalData.progressStatus === TASK_COMPLETE) {
-      if (
-        confirm(
-          '업무 완료시 다른 수정사항은 반영되지 않습니다. 업무를 완료하시겠습니까?',
-        )
-      ) {
-        completeTask();
-      }
-    } else {
-      const data: TaskModForm = modModalData;
+    const data: TaskModForm = modModalData;
 
-      try {
-        updateTaskInputSchema.parse(data);
-      } catch (e: unknown) {
-        setErrorSnackbar((e as ZodError).errors[0].message);
-        return;
-      }
-      updateTask({
-        ...modModalData,
-        assignedUserId: numStrToBigInt(modModalData.assignedUserId),
-      });
+    try {
+      updateTaskInputSchema.parse(data);
+    } catch (e: unknown) {
+      setErrorSnackbar((e as ZodError).errors[0].message);
+      return;
     }
+    updateTask({
+      ...modModalData,
+      assignedUserId: numStrToBigInt(modModalData.assignedUserId),
+    });
   };
 
   return (
@@ -104,7 +75,7 @@ const TaskModModal = () => {
               close={handleClickCloseButton}
               title={title}
               onClickConfirmHandler={handleClickConfirmButton}
-              isUpdating={isUpdating || isCompleting}
+              isUpdating={isUpdating}
             >
               <section className='tablet:w-[450px] mobile:w-[280px] max-h-[500px] mb-10 flex-col mt-5'>
                 <div className='space-y-5 mobile:space-y-3 mx-4 mobile:mx-0 mobile:text-sm'>
