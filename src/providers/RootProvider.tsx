5da5ec5eda618cab404ad27dfb863fc375374a02
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-06-20 21:58:57 +0900

    fix: 서버 queryClient와 브라우저 queryClient 불일치하던 버그 수정 - TanstackQueryClientProvider 제거하고 RootProvider에서 직접 queryClient생성해서 QueryProvider에 주입

--- src/providers/RootProvider.tsx
+++ src/providers/RootProvider.tsx
@@ -2,16 +2,57 @@
 
 import { RecoilRoot } from 'recoil';
 import { ReactNode } from 'react';
-import TanStackQueryClientProvider from '@/providers/TanstackQueryClientProvider';
+import {
+  isServer,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { HttpError } from '@/shared/utils/HttpError';
+import { HttpStatusCode } from 'axios';
 
 type RootProviderProps = {
   children: ReactNode;
 };
 
+const makeQueryClient = () => {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000 * 5,
+        refetchOnWindowFocus: false,
+        retry: 2,
+      },
+      mutations: {
+        throwOnError: (error) => {
+          return (
+            error instanceof HttpError &&
+            (error.status === HttpStatusCode.ServiceUnavailable ||
+              error.status === HttpStatusCode.InternalServerError ||
+              error.status === HttpStatusCode.Unauthorized)
+          );
+        },
+      },
+    },
+  });
+};
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+const getQueryClient = () => {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+};
+
 const RootProvider = ({ children }: RootProviderProps) => {
+  const queryClient = getQueryClient();
+
   return (
     <RecoilRoot>
-      <TanStackQueryClientProvider>{children}</TanStackQueryClientProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </RecoilRoot>
   );
 };
